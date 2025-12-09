cc3b25e49aed4f760dc9a02c48c47f3f5dd30592
Siddesh.k <siddeshk8310@gmail.com>
2025-12-09 13:29:16 +0530

    Add all Python files

---
+++ python/4.py
@@ -0,0 +1,23 @@
+numbers = [1, 2, 3]
+x, y, z = numbers
+print(x, y, z)  # Output: 1 2 3
+name = (1.00,66.99)
+x , y  = name
+print(x , y )  # Output: siddesh niveen
+x = 5
+print(x)
+numbers = [1, 2, 3]
+x, y, z = numbers
+print(x, y, z)  # Output: 1 2 3
+x = 10
+print(x)  # Output: 10
+name = "  siddesh.k    "
+print(name.upper()) 
+name = " naveen "
+print(name.replace("naveen" , "siddesh"))
+seentx = '''siddesh said"hello"
+"naveen said 'hi'''
+print(seentx)
+_name = '''siddesh said "hello"
+naveen said "hi"'''
+print(_name*30)
---
+++ python/7.py
@@ -0,0 +1,31 @@
+item  = ["apple","banana","cherry"]
+#item.remove("cherry")
+#item.pop(-2)
+#item.append("buscuit")
+#i+
+# em.insert(2,"buscuit")
+#item[0] = "bro4" 
+#a = [10,33,66,100,55,66]
+#print(sorted(a))
+#a.sort()
+items = [1,88,99,100]
+sorted_items = sorted(items)
+rev = sorted_items.reverse()
+print(sorted_items)
+r = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+fruits = ["apple", "banana", "cherry"]
+#print(fruits[0])  # Output: apple
+#print(fruits)  # Output: cherry
+fruits[1] = "orange"
+print(fruits)  # Output: ['apple', 'orange', 'cherry']
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+# Accessing elements in a nested list
