1ce045b008a28dd5d97b253b8a773ed7d751e55b
Siddesh.k <siddeshk-dev@users.noreply.github.com>
2025-12-23 08:13:13 +0530

    daily update

---
+++ .vscode/10.py
@@ -0,0 +1,37 @@
+cart = {}
+
+while True:
+    product = input("Enter product name (or exit): ")
+    if product == "exit":
+        break
+
+    price = int(input("Enter price: "))
+    cart[product] = price
+
+print("\nShopping Cart:")
+total = 0
+for item in cart:
+    print(item, ":", cart[item])
+    total += cart[item]
+
+print("Total Bill:", total)
+accounts = {
+    1001: 5000,
+    1002: 8000,
+    1003: 12000
+}
+
+acc_no = int(input("Enter account number: "))
+
+if acc_no in accounts:
+    print("Current Balance:", accounts[acc_no])
+    withdraw = int(input("Enter withdraw amount: "))
+
+    if withdraw <= accounts[acc_no]:
+        accounts[acc_no] -= withdraw
+        print("Withdrawal Successful")
+        print("Remaining Balance:", accounts[acc_no])
+    else:
+        print("Insufficient Balance")
+else:
+    print("Account not found")
